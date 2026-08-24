bd3d7ada5db3537606ec2537ee525d962b272207
Enzo <enzo.erkmann@copel.com>
2026-08-24 10:44:17 -0300

    feat: Ingestão da selic diária feita

--- ingestion_selic_today.py
+++ ingestion_selic_today.py
@@ -1,3 +1,10 @@
 import requests
+import json
 
-re
+# Na parte da url em baixo se usar 432 usa Selic Meta, se usar 1178 usa a Selic Efetiva (aparentemente usama a Efetiva)
+url = "https://api.bcb.gov.br/dados/serie/bcdata.sgs.1178/dados?formato=json&dataInicial=01/01/2023&dataFinal=31/01/2023"
+
+resposta = requests.get(url, verify=False)
+resposta_moggadora = json.dumps(resposta.json(), indent=4, ensure_ascii=False)
+
+print(resposta_moggadora)
